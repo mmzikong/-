f14672972152d257f6ec3fe6d436b019e633f670
SpirngBootDemo <git@github.com:mmzikong/SpirngBootDemo.git>
2024-06-12 13:50:33 +0800

    新课标2024高考数学压轴题 补充注释，经检验，当m>7是结果正确，答案不正确 m=7时，比答案增加两项(6,9),(22,25)，其中两组公差为6

--- src/Exam_2024.java
+++ src/Exam_2024.java
@@ -36,6 +36,7 @@ public class Exam_2024 {
         return newArr;
     }
 
+    //删除数组arr中第i,j,m,n项，由于每组4个元素，每存放一组数组就从原数组中将该数组的元素去除
     private static int[] removeFour(int[] arr, int i, int j,int m, int n){
         int[] newArr = new int[arr.length-4];
         for(int k = 0,l = 0; k < newArr.length; k++,l++){
@@ -54,57 +55,82 @@ public class Exam_2024 {
         System.out.println();
     }
 
+    //对数组进行分割，使每一组都为等差数组，将每一组等差数组都放入队列splitArrays中,若找到一种全为等差数组的队列即为可分数组，返回队列
     private static List<int[]> splitArray(int[] arr) {
+        //用于存放数组的队列
         List<int[]> splitArrays = new ArrayList<>();
 
         int length = arr.length;
-        //共可分为数组长度/4的组数，即原题中的m,也是每组的最大公差
+        //共可分为数组长度/4的组数，即原题中的m,同时要理解每组的最大公差，即每一组的步长最大不超过m
+        //所有的公差组合，每组公差可以为[1,1,1...1]或[1,1,1...2]或...或[m,m,m...m]共m*m种组合
         int numGroups = length / 4;
-        int[] a = new int[numGroups];
+        //数组b,c,d用于存放每次取值后原数组的下标
         int[] b = new int[numGroups];
         int[] c = new int[numGroups];
         int[] d = new int[numGroups];
+        //备份原数组
         int[] oldArr = arr;
-        //所有的公差组合，每组公差可以为[1,1,1...1]或[1,1,1...2]或...或[m,m,m...m]共m*m种组合
+        //存放每次更新数组后的长度
         int newLength = arr.length;
-
+        //记录已经找到几组的等差数列
         int i = 0;
         outerLoopFirst:
         for (int j = 1; j < newLength - 2; j++) {
             outerLoopThird:
             for (int k = j + 1; k < newLength - 1; k++) {
                 for (int l = k + 1; l < newLength; l++) {
+                    //用于存放找到的数组
                     int[] group = new int[4];
+                    //由于每次更新原数组，所以每次取值的第一个数都是新数组的第一个元素
                     group[0] = arr[0];
                     group[1] = arr[j];
                     group[2] = arr[k];
                     group[3] = arr[l];
+                    //公差即为第二个元素与第一个元素的差值，若后面差值大于公差即本次循环之后的元素都不可能构成等差数列
                     if (group[3] - group[2] > group[1] - group[0]) {
+                        //判断是否已经遍历了该数组的所有情况
                         if (j == newLength - 3 && k == newLength - 2 && l == newLength - 1) {
-                            splitArrays.clear();
-                            break outerLoopFirst;
-                        }
-                        break;
-                    }
-                    if (group[2] - group[1] > group[1] - group[0]) {
-                        if (j == newLength - 3 && k == newLength - 2 && l == newLength - 1) {
+                            //返回上一次已经构成的等差数列，重新选值
+                            //删除上一次的选值
+                            splitArrays.removeLast();
+                            arr = oldArr;
+                            i --;
+                            //j,k,l返回上一次的下标
+                            j = b[i];
+                            k = c[i];
+                            l = d[i];
                             if (!splitArrays.isEmpty()){
-                                splitArrays.removeLast();
-                                arr = oldArr;
-                                i --;
-                                j = b[i];
-                                k = c[i];
-                                l = d[i];
+                                //将数组变为上一次选值前的情况
                                 for (int f=0; f<i; f++){
                                     arr = removeFour(arr,0,b[f],c[f],d[f]);
                                 }
                                 newLength = arr.length;
                                 break outerLoopThird;
-                            }
-                            break outerLoopFirst;
+                            }else
+                                break outerLoopFirst;
+                        }
+                        break;
+                    }
+                    if (group[2] - group[1] > group[1] - group[0]) {
+                        if (j == newLength - 3 && k == newLength - 2 && l == newLength - 1) {
+                            splitArrays.removeLast();
+                            arr = oldArr;
+                            i --;
+                            j = b[i];
+                            k = c[i];
+                            l = d[i];
+                            if (!splitArrays.isEmpty()){
+                                for (int f=0; f<i; f++){
+                                    arr = removeFour(arr,0,b[f],c[f],d[f]);
+                                }
+                                newLength = arr.length;
+                                break outerLoopThird;
+                            }else
+                                break outerLoopFirst;
                         }
                         break outerLoopThird;
                     }
+                    //步长大于最大公差，本次循环下都不可能
                     if (group[1] - group[0] > numGroups) {
                         if (!splitArrays.isEmpty()){
                             splitArrays.removeLast();
@@ -123,8 +149,11 @@ public class Exam_2024 {
                             break outerLoopFirst;
                         }
                     }
+                    //判断选中的数组group是否为等差数组
                     if (isArithmeticalSequence(group)) {
+                        //若为等差数组就从原数组arr中将已选的数删除
                         arr = removeFour(arr, 0, j, k, l);
+                        //核查已选数组数列与i是否相等
                         if (arr.length + (i + 1) * 4 == length) {
                             b[i] = j;
                             c[i] = k;
@@ -132,14 +161,20 @@ public class Exam_2024 {
                             i++;
                         }
                         newLength = arr.length;
+                        //将该等差数组加入队列中
                         splitArrays.add(group);
+                        //由于数组被更改，将j,k,l进行重置，由于首先返回到l的循环中，l会自增，所以l为2
                         j = 1;
                         k = 2;
                         l = 2;
+                        //若新数组的长度为0，即所以数以及选择完毕，手动结束循环
                         if (newLength == 0) {
                             break outerLoopFirst;
                         }
-                    } else {
+                    }
+                    //选中的数组不是等差数组
+                    else {
+                        //是否已经遍历了所有情况，返回上一次的选值
                         if (j == newLength - 3 && k == newLength - 2 && l == newLength - 1) {
                             splitArrays.removeLast();
                             arr = oldArr;
@@ -172,7 +207,15 @@ public class Exam_2024 {
                 List<int[]> splitArrays = splitArray(newArr);
                 if (!splitArrays.isEmpty()){
                     count++;
-                    System.out.println("是("+(i+1)+","+(j+1)+")的一一可分数列");
+                    System.out.print("("+(i+1)+","+(j+1)+")的可分数列,分组为：");
+                    for (int[] splitArray : splitArrays) {
+                        System.out.print("(");
+                        for (int element : splitArray) {
+                            System.out.print( element + " ");
+                        }
+                        System.out.print(")");
+                    }
+                    System.out.println();
                 }
             }
         }
@@ -185,7 +228,7 @@ public class Exam_2024 {
     }
 
     public static void main(String[] args) {
-//        int[] arr = creatArray(4);
+//        int[] arr = creatArray(7);
 //        printArray(arr);
 //        arr = removeTwo(arr,2,13);
 //        printArray(arr);
@@ -196,6 +239,10 @@ public class Exam_2024 {
 //            }
 //            System.out.println();
 //        }
-        isDivisible(9);
+        isDivisible(7);
+//        for (int i = 1; i <= 8; i++) {
+//            System.out.println("---------------------- m="+ i +"----------------------");
+//            isDivisible(i);
+//        }
     }
 }
